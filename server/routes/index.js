dbbffe530aee02ec6ee0043e0fdb4f4cc6dda469
andriihorkavyi <andrii.horkavyi@auto.ria.com>
2022-07-25 19:50:00 +0300

    swagger connected

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,13 @@
 const Router = require('koa-router');
+const yamljs = require('yamljs');
+const { koaSwagger } = require('koa2-swagger-ui');
 const serviceController = require('../controllers/serviceController');
 
 const routesPrivate = new Router();
+
+const spec = yamljs.load(`${__dirname}/../../gses2swagger.yaml`);
+
+routesPrivate.get('/docs', koaSwagger({ routePrefix: false, swaggerOptions: { spec } }));
 
 routesPrivate
   // service routes
